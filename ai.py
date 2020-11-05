745e019d7fea71dae7238b303b8d404919f8a6c2
Axelvel <axelvel@live.fr>
2020-11-05 11:12:33 +0100

    :sparkles: Added nextState function

--- ai.py
+++ ai.py
@@ -12,16 +12,35 @@ class State():
         self.b = b #Player B's remaining pawns
 
 
+def nextState(state): #Returns a list of possible next states
+    successors = []
 
-# TODO: create nextState function returning a list of possible next states
-def nextState(state):
-    sucessors = []
+    if (state.a != 0 or state.b != 0): #Game in placing phase
+        print("\n*** Placing phase ***\n")
 
-    if (state.a == 0 and state.b == 0): #Game in placing phase
-        print("Placing phase")
+        for i in range(5): #Iterating through every cell of the board
+            for j in range(5):
+                new_state = State(state.board, state.a, state.b - 1) #Substracting 1 pawn to player B (AI)
+                game.place(new_state, i, j, 2, False)
+                if not (np.array_equal(new_state.board, state.board)):
+                    successors.append(new_state)
+
 
     else: #Game in moving phase
-        print("Moving phase")
+        print("\n*** Moving phase ***\n")
+
+        for i in range(5): #Iterating through every cell of the board
+            for j in range(5):
+                if (state.board[i][j] == 2): #If the case contain a player B's pawn
+                    adjacentSlots = game.getAdjacent(state, i, j)
+
+                    for p in adjacentSlots:
+                        new_state = State(state.board, state.a, state.b)
+                        game.move(new_state, i, j, p[0],p[1], False)
+                        if not (np.array_equal(new_state.board, state.board)):
+                            successors.append(new_state)
+
+    return successors
 
 
 #Empty = 0
@@ -44,13 +63,22 @@ initial_state = State([
 game.show(initial_state)
 
 #Placing White pawn (v = 2) at (0,0)
-game.place(initial_state, 0, 0, 2)
+game.place(initial_state, 0, 0, 2, False)
 
 #Show game board
 game.show(initial_state)
 
 #Moving pawn at (0,0) to (1,1)
-game.move(initial_state, 0, 0, 1, 1)
+game.move(initial_state, 0, 0, 1, 1, False)
 
 #Show game board
 game.show(initial_state)
+
+print("\nPossible next states :")
+
+n = nextState(initial_state)
+
+for i in n: #Printing all the possible next states
+    print("\n")
+    print(i.board)
+    print("\n")
--- game.py
+++ game.py
@@ -1,29 +1,34 @@
 # -*- coding: utf-8 -*-
 
-def place(state, a, b, v):
+def place(state, a, b, v, cflag):
     if (state.board[a][b] != 0): #Position is occupied by another pawn
-        print("This position is occupied. \n")
+        if (cflag):
+            print("This position is occupied. \n")
     else: #Position is not occupied
         state.board[a][b] = v #We assign the value of the pawn to this position
 
-def move(state, a, b, x, y):
+def move(state, a, b, x, y, cflag):
     if (a != x or b != y): #Position and destionation must be different
         if (state.board[x][y] != 0): #Destination position is occupied by another pawn
-            print("This destination is occupied. \n")
+            if (cflag):
+                print("This destination is occupied. \n")
         else: #Destination position is unoccupied
             if (state.board[a][b] == 0): #Initial position is unoccupied
-                print("There is no pawn to move at this position. \n")
+                if (cflag):
+                    print("There is no pawn to move at this position. \n")
             else: #initial position is unoccupied
                 adjacentSlots = getAdjacent(state, a, b)
                 if [x,y] in adjacentSlots:
                     state.board[x][y] = state.board[a][b] #Moving pawn value
                     state.board[a][b] = 0 #Resetting initial position value to 0 (empty)
                 else:
-                    print("Destination is not adjacent to the selected pawn. \n")
+                    if (cflag):
+                        print("Destination is not adjacent to the selected pawn. \n")
     else:
-        print("Initial position and destination must be different. \n")
+        if (cflag):
+            print("Initial position and destination must be different. \n")
 
-def getAdjacent(state, a, b):
+def getAdjacent(state, a, b): #Returns list of adjacent slots
     adjacentSlots = []
     directions = [
         [-1, -1], [-1, 0], [-1, +1],
@@ -40,7 +45,7 @@ def getAdjacent(state, a, b):
 
     return adjacentSlots
 
-def show(state):
+def show(state): #Show the game board at a given state
     print("\n")
     print(state.board)
     print("\n")
