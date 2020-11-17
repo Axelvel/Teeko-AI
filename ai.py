f99ffe8932a09028d03d593259e4a5167bd11737
SophieShirine <50867904+SophieShirine@users.noreply.github.com>
2020-11-17 21:16:51 +0100

    :bulb: Added some comments

--- ai.py
+++ ai.py
@@ -3,6 +3,13 @@
 import numpy as np
 import game
 
+#Empty = 0
+#Black = 1
+#White = 2
+
+"""
+@desc class of the gameboard
+"""
 class State():
 
     def __init__(self, board, a, b):
@@ -11,6 +18,11 @@ class State():
         self.a = a #Player A's remaining pawns
         self.b = b #Player B's remaining pawns
 
+"""
+@desc function that searchs for the possible next states
+@param State $state - the state of the current board Game
+@return array $successors - the list of all the next possible states
+"""
 
 def nextState(state): #Returns a list of possible next states
     successors = []
@@ -18,6 +30,7 @@ def nextState(state): #Returns a list of possible next states
     if (state.a != 0 or state.b != 0): #Game in placing phase
         print("\n*** Placing phase ***\n")
 
+        #In the placing phase, we can place a pawn wherever we want except where there are pawns already
         for i in range(5): #Iterating through every cell of the board
             for j in range(5):
                 new_state = State(state.board, state.a, state.b - 1) #Substracting 1 pawn to player B (AI)
@@ -43,9 +56,7 @@ def nextState(state): #Returns a list of possible next states
     return successors
 
 
-#Empty = 0
-#Black = 1
-#White = 2
+##### MAIN #####
 
 initial_state = State([
             [0, 0, 0, 0, 0],
@@ -60,25 +71,28 @@ initial_state = State([
 ### Examples ###
 
 #Show game board
+print("Initial board :")
 game.show(initial_state)
 
+print("Placing of a white pawn :")
 #Placing White pawn (v = 2) at (0,0)
 game.place(initial_state, 0, 0, 2, False)
-
 #Show game board
 game.show(initial_state)
 
+print("Moving that pawn:")
 #Moving pawn at (0,0) to (1,1)
 game.move(initial_state, 0, 0, 1, 1, False)
-
 #Show game board
 game.show(initial_state)
-
+'''
 print("\nPossible next states :")
-
 n = nextState(initial_state)
 
 for i in n: #Printing all the possible next states
     print("\n")
     print(i.board)
     print("\n")
+'''
+print("Calcul des possible winning")
+w = game.winningCombinations(initial_state,1)
--- game.py
+++ game.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
 
+"""
+@desc function that places a pawn on the board
+@param State $state - the state of the current board Game
+@param int $a,b - the coordinates of the position on which we want to place the pawn
+@param int $v - the value of the pawns (0 : empty slot, 1 : black pawn, 2 : white pawn)
+@param ? $cflag - ?
+"""
+
 def place(state, a, b, v, cflag):
     if (state.board[a][b] != 0): #Position is occupied by another pawn
         if (cflag):
             print("This position is occupied. \n")
     else: #Position is not occupied
         state.board[a][b] = v #We assign the value of the pawn to this position
+
+"""
+@desc function that moves a pawn on the board
+@param State $state - the state of the current board Game
+@param int $a,b - the coordinates of the previous position of the pawns
+@param int $a,b - the coordinates of the wanted position of the pawns
+@param ? $cflag - ?
+"""
 
 def move(state, a, b, x, y, cflag):
     if (a != x or b != y): #Position and destionation must be different
@@ -28,7 +44,14 @@ def move(state, a, b, x, y, cflag):
         if (cflag):
             print("Initial position and destination must be different. \n")
 
-def getAdjacent(state, a, b): #Returns list of adjacent slots
+"""
+@desc function that searchs for the adjacents slots of a pawn
+@param State $state - the state of the current board Game
+@param int $a,b - the coordinates of the position of the pawn
+@return array $adjacentsSlots - list of adjacents slots, described as an array of two int
+"""
+
+def getAdjacent(state, a, b):
     adjacentSlots = []
     directions = [
         [-1, -1], [-1, 0], [-1, +1],
@@ -44,6 +67,44 @@ def getAdjacent(state, a, b): #Returns list of adjacent slots
             adjacentSlots.append([x,y])
 
     return adjacentSlots
+
+'''
+@desc TEST function that searchs for all possible winning combinations. it takes into account the pawns already on the board
+@param State $state - the state of the current board Game
+@param int $player - the player for which we are searching winning combinations
+@return array $winnigCombinations - list of game state in which the player is winning
+'''
+def winningCombinations(state,player):
+    #Serach for all horizontal positions, then vertical, then diagonal, then squares ? and store them into an array
+    winning = [];
+
+
+    blankBoard =[[0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]]
+    #Search for all possible combinations, disregarding the actual state of the gameboard
+    #vertical
+
+    start_line = 0;
+    for i in range(4):
+        for j in range(3):
+            newBoard = [blankBoard]
+            newBoard[i+start_line][j]=1
+            show(newBoard)
+            winning.append(newBoard)
+
+    return winning
+
+
+    #compare thoses combinations to the state of the gameboard and only keep the relevants one
+    #blablabla
+
+"""
+@desc print the gameboard
+@param State $state - the state of the current board Game
+"""
 
 def show(state): #Show the game board at a given state
     print("\n")
