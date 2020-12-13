451c7290e4c3694ec5b7a99207630107e811bdfb
SophieShirine <50867904+SophieShirine@users.noreply.github.com>
2020-12-13 23:09:22 +0100

    :bug: fixed a bug in isWinning() function

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 """
 @desc function that places a pawn on the board
 @param State $state - the state of the current board Game
@@ -8,13 +9,15 @@
 @param ? $cflag - ?
 """
 
-def place(state, a, b, v, cflag):
+def place(state, a, b, cflag):
     if (state.board[a][b] != 0): #Position is occupied by another pawn
         if (cflag):
             print("This position is occupied. \n")
+        return False
     else: #Position is not occupied
-        state.board[a][b] = v #We assign the value of the pawn to this position
-
+        state.board[a][b] = state.t #We assign the value of the pawn to this position
+        state.t *= -1
+        return True
 """
 @desc function that moves a pawn on the board
 @param State $state - the state of the current board Game
@@ -24,25 +27,36 @@ def place(state, a, b, v, cflag):
 """
 
 def move(state, a, b, x, y, cflag):
+
     if (a != x or b != y): #Position and destionation must be different
         if (state.board[x][y] != 0): #Destination position is occupied by another pawn
             if (cflag):
                 print("This destination is occupied. \n")
+            return False
         else: #Destination position is unoccupied
             if (state.board[a][b] == 0): #Initial position is unoccupied
                 if (cflag):
                     print("There is no pawn to move at this position. \n")
+                return False
             else: #initial position is unoccupied
-                adjacentSlots = getAdjacent(state, a, b)
-                if [x,y] in adjacentSlots:
-                    state.board[x][y] = state.board[a][b] #Moving pawn value
-                    state.board[a][b] = 0 #Resetting initial position value to 0 (empty)
+                if (state.board[a][b] == state.t):
+                    adjacentSlots = getAdjacent(state, a, b)
+                    if [x,y] in adjacentSlots:
+                        state.board[x][y] = state.board[a][b] #Moving pawn value
+                        state.board[a][b] = 0 #Resetting initial position value to 0 (empty)
+                        state.t *= -1
+                        return True
+                    else:
+                        if (cflag):
+                            print("Destination is not adjacent to the selected pawn. \n")
+                        return False
                 else:
-                    if (cflag):
-                        print("Destination is not adjacent to the selected pawn. \n")
+                    print("The pawn selected is not one of yours")
+                    return False
     else:
         if (cflag):
             print("Initial position and destination must be different. \n")
+        return False
 
 """
 @desc function that searchs for the adjacents slots of a pawn
@@ -82,7 +96,8 @@ def isWinning(state,player):
     x = 0
     y = 0
     posX = 0
-    poxY = 0
+    posY = 0
+
     while state.board[x][y] != player:
         for x in range(5):
             for y in range(5):
@@ -90,17 +105,17 @@ def isWinning(state,player):
                     posX = x
                     posY = y
                     break
-            if state.board[x][y] == player:
+            if state.board[x][y] ==player:
                 break
         if state.board[x][y] == player:
-            print(state.board[x][y])
+            #print(state.board[x][y])
             break
 
 
     ##HORIZONTAL PAWNS TEST
     compte = 0
     for i in range(posY,5):
-        if state.board[x][y] == player :
+        if state.board[x][y] == player:
             y = y + 1
             compte = compte + 1
         else:
@@ -113,7 +128,7 @@ def isWinning(state,player):
     y = posY
     compte = 0
     for i in range(posX,5):
-        if state.board[x][y] == player :
+        if state.board[x][y] == player:
             compte = compte + 1
             x = x + 1
         else:
@@ -126,7 +141,7 @@ def isWinning(state,player):
     y = posY
     compte = 0
     for i in range(posY,5):
-        if state.board[x][y] == player :
+        if state.board[x][y] == player:
             compte = compte + 1
             x = x + 1
             y = y + 1
@@ -142,7 +157,7 @@ def isWinning(state,player):
     y = posY
     compte = 0
     for i in range(posX,5):
-        if state.board[x][y] == player :
+        if state.board[x][y] == player:
             compte = compte + 1
             x = x + 1
             y = y - 1
@@ -159,15 +174,16 @@ def isWinning(state,player):
     compte = 0
     directions = [[0,+1],[+1,0],[0,-1],[0,0],]
 
-    for i in directions:
-        if state.board[x][y] == player :
-            compte = compte + 1
-            x = x + i[0]
-            y = y + i[1]
-        else:
-            break
-    if(compte == 4):
-        return True
+    if x<4 and y<4 :
+        for i in directions:
+            if state.board[x][y] == player:
+                compte = compte + 1
+                x = x + i[0]
+                y = y + i[1]
+            else:
+                break
+        if(compte == 4):
+            return True
 
     return False
 
