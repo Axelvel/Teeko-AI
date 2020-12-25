c79a8e00ecc65915d2b5b2bac96cbe4bd8d4edb8
SophieShirine <50867904+SophieShirine@users.noreply.github.com>
2020-12-25 23:25:42 +0100

    ✨ Player vs Player and Player versus AI modes added

--- game.py
+++ game.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import os
+import time
+
 
 """
 @desc function that places a pawn on the board
 @param State $state - the state of the current board Game
 @param int $a,b - the coordinates of the position on which we want to place the pawn
-@param int $v - the value of the pawns (0 : empty slot, 1 : black pawn, 2 : white pawn)
-@param ? $cflag - ?
+@param bool $cflag - allow console messages
+@return bool - ??
 """
-
 def place(state, a, b, cflag):
     if (state.board[a][b] != 0): #Position is occupied by another pawn
         if (cflag):
             print("This position is occupied. \n")
         return False
     else: #Position is not occupied
+
         state.board[a][b] = state.t #We assign the value of the pawn to this position
         state.t *= -1
         return True
@@ -22,12 +25,12 @@ def place(state, a, b, cflag):
 @desc function that moves a pawn on the board
 @param State $state - the state of the current board Game
 @param int $a,b - the coordinates of the previous position of the pawns
-@param int $a,b - the coordinates of the wanted position of the pawns
+@param int $x,y - the coordinates of the wanted position of the pawns
 @param bool $cflag - allow console messages
+@return bool - ??
 """
 
 def move(state, a, b, x, y, cflag):
-
     if (a != x or b != y): #Position and destionation must be different
         if (state.board[x][y] != 0): #Destination position is occupied by another pawn
             if (cflag):
@@ -40,6 +43,7 @@ def move(state, a, b, x, y, cflag):
                 return False
             else: #initial position is unoccupied
                 if (state.board[a][b] == state.t):
+
                     adjacentSlots = getAdjacent(state, a, b)
                     if [x,y] in adjacentSlots:
                         state.board[x][y] = state.board[a][b] #Moving pawn value
@@ -58,13 +62,13 @@ def move(state, a, b, x, y, cflag):
             print("Initial position and destination must be different. \n")
         return False
 
+
 """
 @desc function that searchs for the adjacents slots of a pawn
 @param State $state - the state of the current board Game
 @param int $a,b - the coordinates of the position of the pawn
 @return array $adjacentsSlots - list of adjacents slots, described as an array of two int
 """
-
 def getAdjacent(state, a, b):
     adjacentSlots = []
     directions = [
@@ -83,7 +87,6 @@ def getAdjacent(state, a, b):
     return adjacentSlots
 
 '''
-A AMELIORER
 @desc function that verify if the board owns a winning combination
 @param State $state - the state of the current board Game
 @param int $player - number of the player for which we are checking the boardd
@@ -102,7 +105,7 @@ def isWinning(state,player):
                     posX = x
                     posY = y
                     break
-            if state.board[x][y] ==player:
+            if state.board[x][y] == player:
                 break
         if state.board[x][y] == player:
             break
@@ -187,8 +190,92 @@ def isWinning(state,player):
 @desc print the gameboard
 @param State $state - the state of the current board Game
 """
-
-def show(state): 
+def show(state):
     print("\n")
     print(state.board)
     print("\n")
+
+
+"""
+@desc Main function that launchs the game
+@param State $state - the initial state of the game
+"""
+def play(state):
+    print("Lauching game...\n")
+
+    i = 0
+    t = 0
+
+    while (i != 10): #Nombre total de tours
+        os.system('clear')
+        print("\n*** Tour " + str(i) + " ***", flush=True)
+
+        show(state)
+
+
+        if (t == 0): #Player 1's turn
+            print("\nPlayer 1's turn\n", flush=True)
+
+            if (state.a != 0): #Placement phase
+                print("Placement phase: ")
+
+                while True:
+                    try:
+                        x_pos = int(input("Choose x position: "))
+                        y_pos = int(input ("Choose y position: "))
+
+                    except:
+                        print("Sorry, I didn't understand that.")
+                        continue
+
+                    if not(0 <= x_pos <= 4 and 0 <= y_pos <= 4):
+                        print("Please select values from 0 to 4.")
+                        continue
+
+                    if (place(state, x_pos, y_pos, True) == False):
+                        continue
+                    else:
+                        break
+
+                state.a -= 1
+
+                show(state)
+
+                print("Next turn...")
+                time.sleep(3)
+
+            else:
+                print("Moving phase: ")
+
+                while True:
+                    try:
+                        x_pos = int(input("Choose x position: "))
+                        y_pos = int(input ("Choose y position: "))
+
+                        a_pos = int(input("Choose a position: "))
+                        b_pos = int(input ("Choose b position: "))
+
+                    except:
+                        print("Sorry, I didn't understand that.")
+                        continue
+
+                    if not(0 <= x_pos <= 4 and 0 <= y_pos <= 4 and 0 <= a_pos <= 4 and 0 <= b_pos <= 4 ):
+                        print("Please select values from 0 to 4.")
+                        continue
+
+                    if (move(state, x_pos, y_pos, a_pos, b_pos, True) == False):
+                        continue
+                    else:
+                        break
+
+
+                show(state)
+                print("Next turn...")
+                time.sleep(3)
+
+        else: # Player 2's turn
+            print("\nPlayer 2's turn\n", flush=True)
+
+        t = not(t)
+
+        i += 1
