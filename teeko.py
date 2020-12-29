08f0731dcb3a04f936d193590875dba9dfa5b16b
SophieShirine <50867904+SophieShirine@users.noreply.github.com>
2020-12-29 16:46:26 +0100

    :adhesive_bandage: fixed import

--- teeko.py
+++ teeko.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import gameBoard
+import game
 import ai
 
-state = gameBoard.boardGame([
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0]
-        ],1,8)
+state = game.boardGame([
+            [0, 1, -1, 0, 0],
+            [0, 0, 0, 0, -1],
+            [0, 1, -1, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 0, -1]
+        ],1,0)
 
 ai = ai.TeekoAI(state,-1)
 
@@ -18,5 +18,5 @@ while(state.winner()==0):
 
     state.playPlayer()
     state.print()
-    state = ai.playHard(3)
+    state = ai.playMediumOrHard(3,int(1)) #play hard here. 0 for intermediate.
     state.print()
